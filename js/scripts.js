3ce4d20e5f1d1d7964df6640b10a268d8f1b5979
Larry Taylor, Max Scher and Rena Rallis <rena@>
2017-06-14 11:45:50 -0700

    add click function to remove starting die when clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,30 @@
 //business logic in the front
-
+function rollDice(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.random() * (max - min) + min;
+}
 
 //party logic in the back
 $(document).ready(function() {
   $("#inputFields").submit(function(event) {
     event.preventDefault();
+
     var player1 = $("input#playerOne").val();
     var player2 = $("input#playerTwo").val();
 
     $("#nameInput").hide();
     $("#gameBoard").show();
+  });
 
+
+  $(".dice").click(function(event) {
+    event.preventDefault();
+
+    var min = 1;
+    var max = 7;
+    var roll = rollDice(min, max);
+    $(".dice").hide();
+    $("#rollBox").text(roll);
   });
 });
